044197f8549155252cc06fdd4517b2612386b9dc
Rahul <rahulbokhare55@gmail.com>
2026-06-14 05:15:58 +0530

    created user schema for user authentication

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const { required } = require("joi");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+
+
+const userSchema = new Schema({
+    email:{
+        type: String,
+        required:true
+    }
+})
+
+User.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", userSchema);
+
+
+module.exports = User;
